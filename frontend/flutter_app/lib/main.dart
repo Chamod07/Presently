595129c7e8df52a527a4baee8125104466ffca5c
Odara <odara.20230714@iit.ac.lk>
2024-11-30 07:04:41 +0530

    implement the basic structure of task group page , task lsit page should be more implemented

--- frontend/flutter_app/lib/main.dart
+++ frontend/flutter_app/lib/main.dart
@@ -1,27 +1,50 @@
+// import 'package:flutter/material.dart';
+// import 'home_page.dart';
+// import 'camera.dart';
+// import 'summary_page.dart';
+// import 'task_group_page.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Presently App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.purple,
+//       ),
+//       home: HomePage(),
+//       routes: {
+//         '/summary': (context) => SummaryPage(),
+//         '/camera': (context) => RecordingScreen(),
+//         '/task_group': (context) => TaskGroupPage()
+//       },
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'home_page.dart';
-import 'camera.dart';
-import 'summary_page.dart';
+import 'task_group_page.dart'; // Import your TaskGroupPage file
 
 void main() {
-  runApp(MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Presently App',
+      title: 'Task Group Page Test',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: HomePage(),
-      routes: {
-        '/summary': (context) => SummaryPage(),
-        '/camera': (context) => RecordingScreen()
-      },
+      home: TaskGroupPage(), // Remove const here
     );
   }
 }
