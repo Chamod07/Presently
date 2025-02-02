69f80d493a549c8c9fd63a4fd884d2d3a50b209b
Dinuka Sithuranga <153531622+DinukaM1@users.noreply.github.com>
2025-02-02 09:50:14 +0530

    Add path of settings page in navbar

--- frontend/flutter_app/lib/navbar.dart
+++ frontend/flutter_app/lib/navbar.dart
@@ -37,7 +37,7 @@ class NavBar extends StatelessWidget {
           BottomNavigationBarItem(
             icon: Icon(Icons.settings_outlined),
             activeIcon: _circularIcon(Icons.settings),
-            label: '',
+            label: 'Settings',
           ),
         ],
         currentIndex: selectedIndex,
@@ -57,7 +57,7 @@ class NavBar extends StatelessWidget {
               Navigator.pushReplacementNamed(context, '');
               break;
             case 3:
-              Navigator.pushReplacementNamed(context, '');
+              Navigator.pushReplacementNamed(context, '/settings');
               break;
           }
         },
