36b99854a0bc3b99b1100af1c803f42194c5667f
Gajitha1507 <gajitha.20232100@iit.ac.lk>
2025-02-19 09:09:35 +0530

    Modified the sliding feature in summary page

--- frontend/flutter_app/lib/components/summary/graph_display.dart
+++ frontend/flutter_app/lib/components/summary/graph_display.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 
-class GraphDisplay extends StatelessWidget {
+class GraphDisplay extends StatefulWidget {
   const GraphDisplay({super.key});
+
+  @override
+  State<GraphDisplay> createState() => _GraphDisplayState();
+}
+
+class _GraphDisplayState extends State<GraphDisplay> {
   @override
   Widget build(BuildContext context) {
-
     return SizedBox(
       height: 150,
-      child: SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            createGraph(0.7, 'images/Body_Language_Summary.png'),
-            createGraph(0.8, 'images/Audio_Summary.png'),
-            createGraph(0.9, 'images/Emotion_Summary.png'),
-          ]
-        ),
+      child: PageView(
+        children: <Widget> [
+        Center( child:
+            createGraph(0.7, 'images/Body_Language_Summary.png'),),
+        Center( child:
+            createGraph(0.8, 'images/Audio_Summary.png'),),
+        Center( child:
+            createGraph(0.9, 'images/Emotion_Summary.png'),),
+        ],
       ),
     );
   }
+
   Widget createGraph(double score, String image){
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 150.0),
