258b25f91cce97569c13ec264f144a735106b05c
Dinuka Sithuranga <153531622+DinukaM1@users.noreply.github.com>
2025-02-24 07:50:33 +0530

    Scenario selection is now scrollable and title can be added to session

--- frontend/flutter_app/lib/components/scenario_selection/session_provider.dart
+++ frontend/flutter_app/lib/components/scenario_selection/session_provider.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 class SessionProvider with ChangeNotifier{
   String? _selectedPresentationType;
   String? _selectedPresentationGoal;
+  String? _selectedName;
   List<String> _sessions = [];
 
   List<String> get sessions => _sessions;
   String? get selectedPresentationType => _selectedPresentationType;
   String? get selectedPresentationGoal => _selectedPresentationGoal;
+  String? get selectedName => _selectedName;
 
   //session data update
-  void startSession(String presentationType, String presentationGoal){
+  void startSession(String presentationType, String presentationGoal, String sessionName){
     _selectedPresentationType = presentationType;
     _selectedPresentationGoal = presentationGoal;
+    _selectedName = sessionName;
     notifyListeners();
   }
   void addSession(String session){
@@ -24,6 +27,7 @@ class SessionProvider with ChangeNotifier{
   void clearSession(){
     _selectedPresentationType = null;
     _selectedPresentationGoal = null;
+    _selectedName = null;
     notifyListeners();
   }
 }
