792d9c5e65117921dd32d7e2d5e9b6764092e6ad
Gajitha1507 <gajitha.20232100@iit.ac.lk>
2025-03-06 20:10:47 +0530

    added the appbar again and fixed the build card not navigating to summary page problem

--- frontend/flutter_app/lib/components/dashboard/home_page.dart
+++ frontend/flutter_app/lib/components/dashboard/home_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/components/dashboard/navbar.dart';
 import 'package:flutter_app/components/scenario_selection/session_provider.dart';
-import 'package:flutter_app/components/signin_signup/sign_in.dart';
 import 'package:provider/provider.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,8 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final supabase = Supabase.instance.client;
-
   @override
   void initState() {
     super.initState();
@@ -25,39 +20,16 @@ class _HomePageState extends State<HomePage> {
         // title: Text('Hello Mariah,'),
         automaticallyImplyLeading: false,
         actions: [
-          IconButton(
-            icon: CircleAvatar(
-              radius: 40,
-              backgroundImage: NetworkImage(
-                'https://via.placeholder.com/150',
-              ),
-            ),
-            onPressed: () {
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              try {
-                await supabase.auth.signOut();
-                if (context.mounted) {
-                  Navigator.of(context).pushAndRemoveUntil(
-                    MaterialPageRoute(builder: (context) => SignInPage()),
-                    (route) => false,
-                  );
-                }
-              }catch (error){
-                if (context.mounted){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                        content: Text('Error signing out'),
-                        backgroundColor: Colors.red,
-                    ),
-                  );
-                }
-              }
-    }
-          )
+      IconButton(
+      icon: CircleAvatar(
+      radius: 40,
+        backgroundImage: NetworkImage(
+          'https://via.placeholder.com/150',
+        ),
+      ),
+      onPressed: () {
+      },
+    ),
         ],
       ),
       body: Padding(
@@ -67,9 +39,6 @@ class _HomePageState extends State<HomePage> {
           children: [
             Row(
               children: [
-                CircleAvatar(
-                  radius: 30,
-                ),
                 SizedBox(width: 16.0),
                 Expanded(
                   child: Column(
@@ -97,7 +66,6 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
             SizedBox(height: 15.0),
-
             Row(
               children: [
                 ElevatedButton(
@@ -162,7 +130,7 @@ class _HomePageState extends State<HomePage> {
                     itemBuilder: (context, index){
                       return _buildCard(
                         title: sessionProvider.sessions[index],
-                        navigateTo: '/session_page',
+                        navigateTo: '/summary',
                       );
                     },
                   );
