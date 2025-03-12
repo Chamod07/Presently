c3f696ecb4790ed3024f19f6ce4fa78b60c6524e
akilapramod <161842369+akilapramod@users.noreply.github.com>
2025-03-12 20:26:33 +0530

    Update base URL in config for API requests to match local development environment

--- frontend/flutter_app/lib/config/config.dart
+++ frontend/flutter_app/lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
   // Base URL for API requests
-  static const String baseUrl = 'http://192.168.1.5:8000'; // Replace with your actual API URL
+  static const String baseUrl = 'http://10.0.2.2:8000'; // Replace with your actual API URL
   static const String apiPath = '/api';
   
   // Full API URL
